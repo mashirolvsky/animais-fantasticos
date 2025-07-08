c2ad7f84b1b22d5fa6c458fac3b1843fac8c90e3
Lucas de Andrade Rocha <lucas@ecombase.com.br>
2025-07-08 12:08:06 -0300

    refat-animate-scroll: Reestruturado por funções mais performáticas.

--- src/modules/animate-scroll.js
+++ src/modules/animate-scroll.js
@@ -2,34 +2,44 @@ export default class AnimateScroll {
   constructor(sections, className) {
     this.sections = document.querySelectorAll(sections);
     this.className = className;
-    this.halfWindow = window.innerHeight * 0.6;
+    this.halfWindow = Math.floor(window.innerHeight * 0.6);
 
-    this.animateScroll = this.animateScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  verifySection(element) {
-    let sectionTop = element.getBoundingClientRect().top;
-    const isSectionVisible = sectionTop - this.halfWindow < 0;
-    if (isSectionVisible) element.classList.add(this.className);
-    else if (element.classList.contains(this.className))
-      element.classList.remove(this.className);
+  getDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: offset - this.halfWindow,
+      };
+    });
   }
 
-  animateScroll() {
-    this.sections.forEach(section => {
-      this.verifySection(section);
+  checkDistance() {
+    this.distance.forEach(({ element, offset }) => {
+      if (window.pageYOffset > offset) {
+        element.classList.add(this.className);
+      } else if (element.classList.contains(this.className)) {
+        element.classList.remove(this.className);
+      }
     });
   }
 
   addWindowEvent() {
-    this.animateScroll();
-    return window.addEventListener("scroll", this.animateScroll);
+    this.checkDistance();
+    return window.addEventListener("scroll", this.checkDistance);
   }
 
   init() {
     if (this.sections.length) {
+      this.getDistance();
       this.addWindowEvent();
     }
     return this;
+  }
+  stop() {
+    return window.removeEventListener("scroll", this.checkDistance);
   }
 }
