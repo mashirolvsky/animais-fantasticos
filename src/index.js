b12656c7f77efb2f93927a9fa17bfa356771f66f
Mashirolvsky <lucasandraderocha3@gmail.com>
2025-07-07 21:40:56 -0300

    refat-modal: modal transformado de função para classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import TabNav from "./modules/init-tab-navigation.js";
 import SmoothScroll from "./modules/init-smooth-scroll.js";
 import initAnimateScroll from "./modules/init-animate-scroll.js";
 import Accordion from "./modules/init-accordion.js";
-import initModal from "./modules/init-modal-handler.js";
+import Modal from "./modules/modal-handler.js";
 import initTooltip from "./modules/init-tooltip-handler.js";
 import initDropdownMenu from "./modules/init-dropdown-menu-handler.js";
 import initMenuMobile from "./modules/init-menu-mobile.js";
@@ -10,17 +10,12 @@ import initOperation from "./modules/init-operation.js";
 import initFetchAnimais from "./modules/init-fetch-animais.js";
 import initFetchBitcoin from "./modules/init-fetch-bitcoin.js";
 
-const tabNav = new TabNav(
-  "[data-tab='menu'] li",
-  "[data-tab='content'] section"
-);
-
+const tabNav = new TabNav("[data-tab='menu'] li", "[data-tab='content'] section");
 const smoothScroll = new SmoothScroll("[data-menu='suave'] a[href^='#']");
-
 const accordion = new Accordion("[data-anime='accordion'] dt");
+const modal = new Modal("[data-modal='open']", "[data-modal='container']", "[data-modal='close']");
 
 initAnimateScroll();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
@@ -31,3 +26,4 @@ initFetchBitcoin();
 smoothScroll.init();
 accordion.init();
 tabNav.init();
+modal.init()
